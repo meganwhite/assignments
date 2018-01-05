1e9219b4a8521bfb413f4cc9739b8726fbfb64d6
meganwhite <mfw247@gmail.com>
2018-01-05 16:30:17 -0500

    add comments to sayhello.py

--- sayhello.py
+++ sayhello.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python2
+#This is importing the person's name
 import sys
+#This is assigning the input to a variable
 name = sys.stdin.read()
+#This is printing the person's name based on input
 print "Hello " + name + "!"
