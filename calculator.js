f506db98b10929edd0f54c21c2282cfb3dab294d
meganwhite <mfw247@gmail.com>
2018-01-07 14:31:06 -0500

    add functions to calculator

---
+++ calculator.js
@@ -0,0 +1,20 @@
+
+// Multiply
+function multiply(a,b) {
+	return (a * b)
+}
+
+// Add
+function add(a,b) {
+	return (a + b)
+}
+
+// Subtract
+function subtract(a,b) {
+	return (a - b)
+}
+
+// Divide
+function divide(a,b) {
+	return (a / b)
+}
