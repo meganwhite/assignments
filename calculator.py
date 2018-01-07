2fbf9d28e59108c4175afe99f326d997fa263099
meganwhite <mfw247@gmail.com>
2018-01-07 14:25:17 -0500

    added functions for squaring, cubing, and squaring n times.

--- calculator.py
+++ calculator.py
@@ -15,6 +15,26 @@ def subtract(a,b):
 def divide(a,b):
 	return a/b
 
+# Square
+def square(a):
+	return a * a
+
+# Cube
+def cube(a):
+	return a * a * a
+
+# Square n times
+def square_n_times(number,n):
+	i = 1
+	while i <= n:
+		number = square(number)
+		i += 1
+	return number
+
 print "I'm going to use the calculator functions to divide 30 by 5"
 x = divide(30,5)
 print x
+
+print "I'm going to use the caluclator to square 2 3 times."
+y = square_n_times(2,3)
+print y
